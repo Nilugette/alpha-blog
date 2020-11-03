e485176e842db5c572108b6f8c44cea48cfca0db
Nilugette <nilugette@yahoo.fr>
2020-11-03 13:34:44 +0100

    Add login form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new
+    end
+
+    def destroy 
+        @user = User.new
+    end
+
+end
