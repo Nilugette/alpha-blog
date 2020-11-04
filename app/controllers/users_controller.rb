f0ce183d15ed90d616d52f1087d2172c9467a8ed
Nilugette <nilugette@yahoo.fr>
2020-11-04 10:49:44 +0100

    Style modification navbar and home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up !"
+            flash[:notice] = "Welcome to Splash #{@user.username}, you have successfully signed up !"
             redirect_to articles_path
         else
             render 'new'
