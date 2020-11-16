d4b401948d0c3666c16c120a7bb5f61e50012fe0
Nilugette <nilugette@yahoo.fr>
2020-11-16 10:17:54 +0100

    Update navigation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
     before_action :require_admin, except: [:index, :show]
+    before_action :require_user
 
     def new
         @category = Category.new
